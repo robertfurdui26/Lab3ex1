389f8c237d0782ea12c8ae801140d31e20e41b97
robertfurdui26 <125736128+robertfurdui26@users.noreply.github.com>
2023-07-03 14:20:55 +0300

    Add project files.

---
+++ Lab3ex1/Program.cs
@@ -0,0 +1,15 @@
+﻿//Ex 1
+//Scrieti o functie care va calcula suma cifrelor unui numar.
+//folosesc 2022 sunt deja in functia Main asa ca nu o sa mai scriu :))
+
+
+
+Console.WriteLine(AddTwoNumbers(5, 15));
+
+
+
+
+static int AddTwoNumbers(int a, int b)
+{
+    return a + b;
+}
